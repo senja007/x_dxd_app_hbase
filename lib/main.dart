b37e514da13fc426748c195f3c2a0c434a0132e9
senja <aswinaditya007@gmail.com>
2023-08-30 10:52:06 +0700

    input tanggal
    pengimputan tanggal

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'package:intl/intl.dart';
 
 import 'app/routes/app_pages.dart';
@@ -12,6 +14,7 @@ Future<void> main() async {
   await GetStorage.init();
   final box = GetStorage();
   Intl.defaultLocale = 'id_ID';
+  initializeDateFormatting('id_ID', null).then((_) {
 
   runApp(
     GetMaterialApp(
@@ -24,4 +27,5 @@ Future<void> main() async {
       navigatorObservers: [BotToastNavigatorObserver()],
     ),
   );
+  });
 }
