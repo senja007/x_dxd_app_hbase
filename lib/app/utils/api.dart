ef10457443e94c00f7e55cf2889782a1f1ecbe2b
senja <aswinaditya007@gmail.com>
2023-11-28 09:48:59 +0700

    ganti logo dan nama
    ganti nama , logo dan y

--- lib/app/utils/api.dart
+++ lib/app/utils/api.dart
@@ -4,8 +4,8 @@ class SharedApi {
   // String imageUrl = "http://192.168.5.245:8080/";
   // String baseUrl = "http://192.168.5.245:8080/api";
 
-  String imageUrl = "http://192.168.0.230:8080/downloadFile/";
-  String baseUrl = "http://192.168.0.230:8080/api";
+  String imageUrl = "http://23.150.24.250:50183/downloadFile/";
+  String baseUrl = "http://23.150.24.250:50183/api";
 
   Map<String, String> getToken() {
     final box = GetStorage();
